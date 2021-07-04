3c6b575f0ff6d205d7faafd2613ebfbdba2ec1b1
Wesley Werner <wesley.werner@gmail.com>
2021-07-05 07:57:54 +1000

    Include build script, gitignore.

--- build/package.bat
+++ build/package.bat
@@ -1,1 +1,2 @@
-"c:\Program Files\7-Zip\7z.exe" a dopetrader.love ..\src\*
+"c:\Program Files\7-Zip\7z.exe" a build\dopetrader.love .\src\*
+ C:\Android\platform-tools\adb.exe push build\dopetrader.love /storage/emulated/0/Download
